a01001050afdc373fd839386fc75c377de0bd61e
ritika-neema <ritikaneema@gmail.com>
2018-06-11 10:50:45 +0530

    Added parallax scrolling and about us section

--- app/src/main/java/com/ritikaneema/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/ritikaneema/example/helloandroid/MainActivity.java
@@ -1,28 +1,33 @@
 package com.ritikaneema.example.helloandroid;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import java.util.Timer;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
 
+    public int autoslideTracker;
     ViewPager viewPager;
     LinearLayout sliderDotspanel;
     private int dotscount;
     private ImageView[] dots;
-    public int autoslideTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ObservableScrollView observableScrollView = (ObservableScrollView) findViewById(R.id.observable_scrollview);
+        observableScrollView.setScrollViewCallbacks(this);
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
@@ -30,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         dotscount = viewPagerAdapter.getCount();
         dots = new ImageView[dotscount];
-        for(int i = 0; i < dotscount; i++){
+        for (int i = 0; i < dotscount; i++) {
 
             dots[i] = new ImageView(this);
             dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_default));
@@ -52,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
 
-                for(int i = 0; i< dotscount; i++){
+                for (int i = 0; i < dotscount; i++) {
                     dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.tab_indicator_default));
                 }
 
@@ -71,6 +76,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        viewPager.setTranslationY(scrollY / 3);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+
+    }
+
     public class MyTimerTask extends TimerTask {
 
         @Override
@@ -80,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
 
-                    if(autoslideTracker <= 3) {
+                    if (autoslideTracker <= 3) {
                         if (viewPager.getCurrentItem() == 0) {
                             viewPager.setCurrentItem(1);
                             autoslideTracker++;
